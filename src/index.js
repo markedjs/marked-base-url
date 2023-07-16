ada72a30b93cb5e6247f7ff40afedfe51fae023a
Thanh Ba Nguyen <btnguyen2k@gmail.com>
2023-07-16 16:28:34 +1000

    fix: allow relative base (#58)
    Co-authored-by: Thanh Nguyen <thanhn@microsoft.com>

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
-export function baseUrl(base) {
+export function baseUrl(baseUrl) {
   // extension code here
 
+  const reAbsUrl = /^[\w+]+:\/\//;
+  baseUrl = baseUrl.trim().replaceAll(/[\/]+$/g, '/'); // if multiple '/' at the end, just keep one
   return {
     walkTokens(token) {
       if (!['link', 'image'].includes(token.type)) {
         return;
       }
 
-      token.href = new URL(token.href, base).href;
+      if (reAbsUrl.test(token.href)) {
+        // keep the URL intact if absolute
+        return;
+      }
+      if (!reAbsUrl.test(baseUrl)) {
+        // baseUrl is not absolute
+        if (token.href.startsWith('/')) {
+          // the URL is from root
+          return;
+        }
+        try {
+          const baseUrlFromRoot = baseUrl.startsWith('/');
+          const dummy = 'http://__dummy__';
+          const temp = new URL(token.href, new URL(baseUrl, dummy)).href;
+          token.href = temp.slice(dummy.length + (baseUrlFromRoot ? 0 : 1));
+        } catch (e) {
+          // ignore
+        }
+      } else {
+        try {
+          token.href = new URL(token.href, baseUrl).href;
+        } catch (e) {
+          // ignore
+        }
+      }
     }
   };
 }
