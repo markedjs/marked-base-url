6b5cf8e5a4a22eb669e2a53e19cbb6380abb0579
sinotaotao <50691262+sinotaotao@users.noreply.github.com>
2023-08-10 14:04:10 +0800

    fix: add types (#77)
    * add index.d.ts
    
    * update package-lock.json file
    
    * TestTypes automation
    
    ---------
    
    Co-authored-by: TaoTao-GStarCAD <taotao@gstarcad.com>

---
+++ types_test/index.test-d.ts
@@ -0,0 +1,12 @@
+import {baseUrl} from 'marked-base-url';
+import {MarkedExtension} from "marked";
+import {expectError, expectType} from 'tsd';
+
+// Simple call
+baseUrl("TestUrl");
+
+// Minimal synchronous options argument
+expectType<MarkedExtension>(baseUrl("TestUrl")) ;
+
+// Invalid argument - missing baseUrl
+expectError(baseUrl());
